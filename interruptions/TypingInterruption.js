7e82359074a07159e0f778bb17bde9aae8f75342
michaelclement <themichaelclement@gmail.com>
2022-10-10 17:16:24 -0500

    finished copy/paste on typinginterruption

--- interruptions/TypingInterruption.js
+++ interruptions/TypingInterruption.js
@@ -4,6 +4,7 @@ class TypingInterruption {
         this.inputs = null;
         this.improveUX = improveUX; // Used to determine if we include HCI improvements
         this.words = ["apple", "orange", "apricot", "banana", "acorn", "squash"];
+        this.clipboard = '';
         this.init();
     }
 
@@ -14,7 +15,6 @@ class TypingInterruption {
     renderTemplate() {
         let template = document.createElement('template');
         // TODO: Prevent right-clicking so user can't copy/paste by default
-
         let content = `
             <div id="interruption-inner-container"
                 class="flex m-0 mx-auto justify-center align-center h-full flex-col w-[400px]">
@@ -31,6 +31,18 @@ class TypingInterruption {
         let inputs = template.content.querySelectorAll("input[type='text']");
         inputs.forEach(input => input.oninput = (e) => this.updateVal(e));
         this.inputs = inputs;
+
+        // Set up the copy buttons:
+        let cpBtns = template.content.querySelectorAll(".copy-btn");
+        cpBtns.forEach(b => b.onclick = (e) => this.clipboard = e.target.title);
+
+        let pstBtns = template.content.querySelectorAll(".paste-btn");
+        pstBtns.forEach(b => b.onclick = (e) => {
+            let inputField = document.getElementById(e.target.title);
+            inputField.value = this.clipboard;
+            // dispatch an event so it triggers the validity check
+            inputField.dispatchEvent(new Event('input')); 
+        });
 
         return template;
     }
@@ -60,20 +72,22 @@ class TypingInterruption {
               <label for="s${i}" id="s${i}-val" class="px-[7px] flex flex-row items-center
               w-[110px] bg-zinc-500 text-white rounded-md m-0 mx-auto
               p-[3px] flex justify-between"><span>${this.words[choice]}</span>
-                ${this.improveUX ? "\
-                  <button class='rounded-md bg-white text-zinc-500 mx-[5px] px-[8px] pb-[2px] flex content-center align-center'>\
-                  <span class='material-icons'>content_copy</span>\
-                </button>" : ""}
+                ${this.improveUX ? `\
+                  <button class='hover:shadow-lg copy-btn rounded-md bg-white text-zinc-500 mx-[5px] px-[8px] pb-[2px] flex content-center align-center'\
+                    title='${this.words[choice]}'>\
+                  <span class='material-icons' title='${this.words[choice]}'>content_copy</span>\
+                </button>` : ""}
               </label>
               <input id="s${i}" 
                      type="text"
                      oninput="updateVal(this)"
                      class="w-[calc(100%-180px)] border border-zinc-500 rounded-md
                      mx-[10px]">
-              ${this.improveUX ? "\
-                <button class='rounded-md text-zinc-500 mx-[5px] px-[8px] pb-[2px] flex content-center align-center'>\
-                <span class='material-icons'>content_paste</span>\
-              </button>" : ""}
+              ${this.improveUX ? `\
+                <button class='hover:shadow-lg paste-btn rounded-md text-zinc-500 mx-[5px] \
+                px-[8px] pb-[2px] flex content-center align-center' title='s${i}'>\
+                <span class='material-icons' title='s${i}'>content_paste</span>\
+              </button>` : ""}
               </label>
             `
         });
